6dc1965dd8d3001d83b8ce78fc26d13f3fb20ad2
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-08 12:59:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,24 +4,24 @@ print("""[0] pip\n[1] pip3\nWhich one do you use?""")
 c = input(">>>: ")
 
 if c == "0":
+     os.system("pip install requests")
+     os.system("pip install progressbar")
+     os.system("pip install randomstring")
      os.system("apt-get install nodej-lts")
      os.system("apt-get install npm")
      os.system("npm install os")
      os.system("npm install sys")
      os.system("npm install socket")
-     os.system("npm install threading")
-     os.system("npm install random")
-     os.system("npm install string")
      
 elif c == "1":
+     os.system("pip3 install requests")
+     os.system("pip3 install progressbar")
+     os.system("pip3 install randomstring")
      os.system("apt-get install nodej-lts")
      os.system("apt-get install npm")
      os.system("npm install os")
      os.system("npm install sys")
      os.system("npm install socket")
-     os.system("npm install threading")
-     os.system("npm install random")
-     os.system("npm install string")
      print("Done.")
 
 
