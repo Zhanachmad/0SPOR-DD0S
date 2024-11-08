3b7d392077f0aa75a23a1c27e8e66a9343225dc4
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-08 13:22:34 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,16 +7,11 @@ if c == "0":
      os.system("pip install requests")
      os.system("pip install progressbar")
      os.system("pip install randomstring")
-     os.system("apt-get install nodej-lts")
-     os.system("apt-get install npm")
-     os.system("npm install os")
+     
 elif c == "1":
      os.system("pip3 install requests")
      os.system("pip3 install progressbar")
      os.system("pip3 install randomstring")
-     os.system("apt-get install nodej-lts")
-     os.system("apt-get install npm")
-     os.system("npm install os")
      print("Done.")
 
 
